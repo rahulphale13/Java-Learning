aacdb251b25e05b240059d4cbf8c0456c1150842
Ram Phale <rahulphale13@gmail.com>
2026-08-14 23:47:26 +0530

    Create Variables.java
    Add variables example

---
+++ Variables.java
@@ -0,0 +1,16 @@
+class Variables {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean passed = true;
+        String name = "Rahul";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
