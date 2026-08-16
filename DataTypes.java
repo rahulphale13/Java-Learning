81559676ebd94ffd18c915fc556b36d6cbfb5a3d
Ram Phale <rahulphale13@gmail.com>
2026-08-16 10:35:03 +0530

    Create DataTypes.java
    Add Data types example

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+class DataTypes {
+    public static void main(String[] args) {
+
+        int number = 25;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Rahul";
+
+        System.out.println("Integer: " + number);
+        System.out.println("Double: " + price);
+        System.out.println("Character: " + grade);
+        System.out.println("Boolean: " + isStudent);
+        System.out.println("String: " + name);
+    }
+}
