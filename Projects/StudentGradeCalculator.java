c086c89618b5fc21b7b6ccb52d18baa267cd150c
Ram Phale <rahulphale13@gmail.com>
2026-08-18 21:01:20 +0530

    Add marks input and total calculation

--- Projects/StudentGradeCalculator.java
+++ Projects/StudentGradeCalculator.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 
 class StudentGradeCalculator {
-     public static void main (String[] args){
-         Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
 
-         System.out.println("=== Student Grade Calculator ===");
+        Scanner sc = new Scanner(System.in);
 
-         System.out.println("Enter Student Name: ");
-         String name = sc.nextLine();
+        System.out.println("=== Student Grade Calculator ===");
 
-         System.out.println("Hello, "+name + "!");
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
 
-         sc.close();
-         
+        int[] marks = new int[5];
 
-     }
+        System.out.println("Enter marks for 5 subjects:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+        }
+
+        int total = 0;
+
+        for (int i = 0; i < 5; i++) {
+            total = total + marks[i];
+        }
+
+        System.out.println("\nHello, " + name + "!");
+        System.out.println("Total Marks: " + total);
+
+        sc.close();
+    }
 }
