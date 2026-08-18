5ccfe537f6a7a90058a3169a62f95b82fafd5c94
Ram Phale <rahulphale13@gmail.com>
2026-08-18 21:13:24 +0530

    Add constructor example

---
+++ Student.java
@@ -0,0 +1,23 @@
+class Student {
+
+    String name;
+    int age;
+
+    // Constructor
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Rahul", 20);
+
+        s1.display();
+    }
+}
