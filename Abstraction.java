70a5a111089e139e2a79f364d2fd8f9b7d88d213
Ram Phale <rahulphale13@gmail.com>
2026-08-24 07:43:34 +0530

    Add Abstraction example

---
+++ Abstraction.java
@@ -0,0 +1,27 @@
+abstract class Animal {
+
+    abstract void sound();
+
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Abstraction {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.sound();
+        d.eat();
+    }
+}
