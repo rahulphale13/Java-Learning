c63f7cfd8b6a17ee8297617404462bf10cbc2446
Ram Phale <rahulphale13@gmail.com>
2026-08-25 20:13:18 +0530

    Add Interface example

---
+++ Interface.java
@@ -0,0 +1,21 @@
+interface Animal {
+
+    void sound();
+}
+
+class Dog implements Animal {
+
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class InterfaceExample {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.sound();
+    }
+}
