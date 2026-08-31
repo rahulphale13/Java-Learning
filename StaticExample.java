7a03a48ab35e4f4cd2701fd6915e7f1101cf98aa
Ram Phale <rahulphale13@gmail.com>
2026-08-31 22:25:22 +0530

    Add Static example

---
+++ StaticExample.java
@@ -0,0 +1,21 @@
+class StaticExample {
+
+    static int count = 0;
+
+    StaticExample() {
+        count++;
+    }
+
+    static void displayCount() {
+        System.out.println("Objects created: " + count);
+    }
+
+    public static void main(String[] args) {
+
+        StaticExample s1 = new StaticExample();
+        StaticExample s2 = new StaticExample();
+        StaticExample s3 = new StaticExample();
+
+        StaticExample.displayCount();
+    }
+}
