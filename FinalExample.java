8b6d77fc09f7944e1bf92ee921c9b21ba691d2ee
Ram Phale <rahulphale13@gmail.com>
2026-09-02 22:45:48 +0530

    Add Final keyword example

---
+++ FinalExample.java
@@ -0,0 +1,15 @@
+class FinalExample {
+
+    final int MAX_MARKS = 100;
+
+    void display() {
+        System.out.println("Maximum Marks: " + MAX_MARKS);
+    }
+
+    public static void main(String[] args) {
+
+        FinalExample obj = new FinalExample();
+
+        obj.display();
+    }
+}
