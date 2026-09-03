93f445a2f938ca2cae470e9748be06febf39e6b7
Ram Phale <rahulphale13@gmail.com>
2026-09-03 20:16:46 +0530

    Add Packages example

---
+++ PackageExample.java
@@ -0,0 +1,10 @@
+import mypackage.Student;
+
+class PackageExample {
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+        s.display();
+    }
+}
---
+++ mypackage/Student.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class Student {
+
+    public void display() {
+        System.out.println("Student class from mypackage");
+    }
+}
